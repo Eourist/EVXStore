ca098f18bcefd252c0ed0551e4215217a9b1a4cd
Eourist <50326501+Eourist@users.noreply.github.com>
2019-08-07 17:43:13 -0300

    asda

--- application/views/header.php
+++ application/views/header.php
@@ -4,6 +4,6 @@
 	<meta charset="UTF-8">
 	<title>Login xD</title>
 	<link rel="stylesheet" href="<?php echo base_url() ?>Assets/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	
 </head>
 <body>
