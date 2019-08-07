b807d1d8522edbe717ee0f65f6a884c8a88ef292
Eourist <50326501+Eourist@users.noreply.github.com>
2019-08-07 17:42:38 -0300

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<title>Login</title>
+	<title>Login xD</title>
 	<link rel="stylesheet" href="<?php echo base_url() ?>Assets/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
