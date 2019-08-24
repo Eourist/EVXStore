71d11672f229d4d1308723017f738fec3a43cc6b
Eourist <50326501+Eourist@users.noreply.github.com>
2019-08-24 20:47:39 -0300

    asdasd

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,7 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> -->
+<script src="<?php echo base_url();?>/Assets/js/jquery-3.4.1.min.js"></script>
+<script src="<?php echo base_url();?>/Assets/js/popper.min.js"></script>
+<script src="<?php echo base_url();?>/Assets/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url();?>/Assets/js/phaser.js"></script>
 <script src="<?php echo base_url();?>/Assets/js/scripts.js"></script>
 
